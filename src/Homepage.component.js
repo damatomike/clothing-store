88674a7fb83ae22f531ca9f636dd0dae294242d1
Mike-Damato <damatomike@gmail.com>
2020-04-19 13:59:22 -0400

    Add - Add homepage component & basic styling

---
+++ src/Homepage.component.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './Homepage.style.scss';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Title</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Sneakers</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Accessories</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
