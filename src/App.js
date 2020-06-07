8a46e38f490070b345aa4cd10cded96c2508943d
Mike-Damato <damatomike@gmail.com>
2020-06-07 19:58:33 -0400

    Add - SignIn Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from './components/pages/hompage/Homepage.component';
 import ShopPage from './components/pages/shop/ShopPage.component';
 import Header from './components/Header/Header.component';
+import SignInAndRegister from './components/SignInRegister/SignInRegister';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndRegister} />
       </Switch>
     </div>
   );
---
+++ src/components/SignInRegister/SignInRegister.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SignIn from '../SignIn/SignIn';
+
+import './SignInRegister.scss';
+
+const SignInRegister = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInRegister;
