bb4d2b8445890306dd7e48d0466a2f4f3ccfd774
Mike-Damato <damatomike@gmail.com>
2020-07-09 15:58:31 -0400

    Fix - Sign in button & sign in & sign out in header logic

--- src/App.js
+++ src/App.js
@@ -4,20 +4,42 @@ import Homepage from './components/pages/hompage/Homepage.component';
 import ShopPage from './components/pages/shop/ShopPage.component';
 import Header from './components/Header/Header.component';
 import SignInAndRegister from './components/SignInRegister/SignInRegister';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndRegister} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndRegister} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -3,9 +3,12 @@ import React from 'react';
 import './CustomButton.styles.scss';
 
 const CustomButton = (props) => {
-  const { children, ...otherProps } = props;
+  const { children, isGoogleSignIn, ...otherProps } = props;
   return (
-    <button className="custom-button" {...otherProps}>
+    <button
+      className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+      {...otherProps}
+    >
       {children}
     </button>
   );
