a715aaac1cb335286303f2fc79edf8e8725b58ad
Mike-Damato <damatomike@gmail.com>
2020-07-12 13:09:22 -0400

    Add - Cart Items Selectors & Refactor CartDropDown & CartItem Component to memoize state using select

--- src/components/CartDropDown/CartDropDown.js
+++ src/components/CartDropDown/CartDropDown.js
@@ -3,6 +3,7 @@ import './CartDropDown.styles.scss';
 import CustomButton from '../CustomButton/CustomButton';
 import CartItem from '../CartItem/CartItem';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart-selectors';
 
 const CartDropDown = ({ cartItems }) => {
   return (
@@ -17,8 +18,8 @@ const CartDropDown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
