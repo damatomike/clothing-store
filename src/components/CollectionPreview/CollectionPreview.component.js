af3cdafae728b58bf61262fa00a2096951c1a680
Mike-Damato <damatomike@gmail.com>
2020-07-15 16:17:10 -0400

    Fix - Links on Shop Page & Hide Logger on production

--- src/components/CollectionPreview/CollectionPreview.component.js
+++ src/components/CollectionPreview/CollectionPreview.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CollectionItem from '../Collection-Item/CollectionItem.component';
+import { Link } from 'react-router-dom';
 
 import './CollectionPreview.styles.scss';
 
@@ -8,9 +9,12 @@ const CollectionPreview = (props) => {
   const itemsList = items
     .filter((item, idx) => idx < 4)
     .map((item) => <CollectionItem key={item.id} item={item} />);
+
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1 className="title">
+        <Link to={`/shop/${title.toLowerCase()}`}>{title.toUpperCase()}</Link>
+      </h1>
       <div className="preview">{itemsList}</div>
     </div>
   );
